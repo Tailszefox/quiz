79f82e152a0ba8cd6244ce80e12c95ed8ab260cc
Tailszefox <tails@tailszefox.no-ip.com>
2015-07-21 12:25:39 +0200

    Changement du format du fichier de config

--- mysql.php
+++ mysql.php
@@ -1,6 +1,6 @@
 <?php
-$config = file('config.txt', FILE_IGNORE_NEW_LINES);
-mysql_connect($config[0], $config[1], $config[2]);
+require("config.php");
+mysql_connect($mysql_hostname, $mysql_username, $mysql_password);
 mysql_query("SET CHARACTER SET utf8");
 mysql_query("SET NAMES utf8");
 mysql_select_db("quiz");
